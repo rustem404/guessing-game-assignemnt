5a3294d648558ea6b582b8b5dd2a18357f6a1494
rustem404 <rustem404@gmail.com>
2020-12-28 20:06:57 +0300

    Updated script and readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # This is a Guessing Game Assignment
 ## Last Run
 'README.md' file modification was on:
-2020-12-22 20:54:52.647632628 +0300
+2020-12-28 20:05:37.994186360 +0300
 ## Number of Lines
 Guessing Game script contains following **number of lines**:
-28
+33
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,28 +1,33 @@
-# Notes:
-# 1) Variables names are intentionally left global to simplify script
-# 2) ls -Fa is taken in respect to POSIX notation (ls -pa also works)
-# 3) Script does not check if value entered is integer
+# Note: Variables names are intentionally left global to simplify script
+
+function input_check {
+read response
+if ! [[ "$response" =~ ^[0-9]+$ ]]
+then echo "Wrong user input. Please enter decimal integer:"
+     input_check
+fi
+}
 
 function compare_values {
 	if [[ $response -gt $actual_value ]]
 then
 	echo "There are not that many files. Please try again:"
-	read response
+	input_check
 elif [[ $response -lt $actual_value ]]
 then
 	echo "You're wrong, there are more files exist in this directory. Please try again:"
-	read response
+	input_check
 fi
 }
 
-
 echo "How many files are in current directory? Please type in an integer:"
-read response
-actual_value=$(ls -Fa | grep -v "/" | wc -l)
+input_check
+
+actual_value=$(ls | wc -l)
 
 while [[ $response -ne $actual_value ]]
 do
 compare_values
 done
 
-echo "It seems to be a case that you are right this time! Good job!"
+echo "It seems to be a case that you are right this time: Number of files is $actual_value and you have entered $response! Good job!"
